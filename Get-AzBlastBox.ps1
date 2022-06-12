79c522d7a43acdbba92a4a94f19004e7d8d77386
mellonaut <rleeazw@outlook.com>
2022-06-11 20:36:38 -0400

    Networking done
    Test again tomorrow but almost everything is there. The functions for nsg could use some work. Cleaned up the old gallery and the old way of getting creds for the machine. Need to make a switch for the different images

--- Get-AzBlastBox.ps1
+++ Get-AzBlastBox.ps1
@@ -1,7 +1,5 @@
 ﻿Import-Module Az
 
-$AdminUsername = Get-Content "./blastuser.api"
-$AdminPassword = Get-Content "./blastpass.api"
 $location = "East US"
 $subscription = (Get-AzSubscription -TenantId $tenant | where-object -Property "State" -eq "Enabled"| select-object -Property Id)
 $VMName = 'BlastBox'
@@ -12,7 +10,6 @@ $pubName = -join("$VMName","-IP")
 $nsgName = -join("$VMName","-NSG")
 $subnetName = -join("$VMName","-Subnet")
 $gallery1 = Get-AzGallery -ResourceGroupName 'Images' -Name 'DevBoxes'
-$gallery2 = Get-AzGallery -ResourceGroupName 'mellonaut' -Name 'malwaremachines'
 $Server2019 = get-azgalleryimageversion -galleryname $gallery1.Name -resourcegroupname $gallery1.ResourceGroupName -galleryImageDefinitionName 'DevServer2019'
 $VS2019 = get-azgalleryimageversion -galleryname $gallery1.Name -resourcegroupname $gallery1.ResourceGroupName -galleryImageDefinitionName 'VS2019'
 $MalwareDev = get-azgalleryimageversion -galleryname $gallery1.Name -resourcegroupname $gallery1.ResourceGroupName -galleryImageDefinitionName 'MalwareDev'
@@ -22,56 +19,80 @@ $CloudDev = get-azgalleryimageversion -galleryname $gallery1.Name -resourcegroup
 $image = $MalwareDev
 
 # Check to see if the resource group exists, if it doesn't it will create it. If it does, the script will ask if you want to add it into the existing group or not.
-$rg = if(!(Get-AzResourceGroup -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue))
-  { New-AzResourceGroup -name $resourceGroupName -location $location }
-  # Using our current host's public ip, we create and open NSG rules allowing RDP, SSH and HTTP/s traffic from that source address 
-function Allow-RDP{
+function Create-RG {
   [CmdletBinding()]
   Param(
       [Parameter(Mandatory)]
-      [String] $ip 
+      [String]$resourceGroupName,
+      [Parameter(Mandatory)]
+      [String]$location
   )
-  New-AzNetworkSecurityRuleConfig -Name rdp-rule -Description "Allow RDP" `
-    -Access Allow -Protocol Tcp -Direction Inbound -Priority 101 -SourceAddressPrefix `
-    "$ip" -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389
+  if(!(Get-AzResourceGroup -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue))
+  { 
+    New-AzResourceGroup -name $resourceGroupName -location $location 
+  }
+}
+$rg = Create-RG $resourceGroupName $location
+  # Using our current host's public ip, we create and open NSG rules allowing RDP, SSH and HTTP/s traffic from that source address 
+function Allow-RDP {
+  [CmdletBinding()]
+  Param(
+      [Parameter(Mandatory)]
+      [String]$ip 
+  )
+  if(!(Get-AzNetworkSecurityRuleConfig -Name rdp-rule -ErrorAction SilentlyContinue))
+    {
+      New-AzNetworkSecurityRuleConfig -Name rdp-rule -Description "Allow RDP" `
+      -Access Allow -Protocol Tcp -Direction Inbound -Priority 101 -SourceAddressPrefix `
+      "$ip" -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389
+    }
 }
 $rule1 = Allow-RDP $myip
-
-function Allow-HTTP{
+function Allow-HTTP {
   [CmdletBinding()]
   Param(
       [Parameter(Mandatory)]
       [String] $ip 
   )
-  New-AzNetworkSecurityRuleConfig -Name http-rule -Description "Allow HTTP" `
-    -Access Allow -Protocol Tcp -Direction Inbound -Priority 102 -SourceAddressPrefix `
-    "$ip" -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 80
+  if(!(Get-AzNetworkSecurityRuleConfig -Name http-rule -ErrorAction SilentlyContinue))
+    {
+      New-AzNetworkSecurityRuleConfig -Name http-rule -Description "Allow HTTP" `
+      -Access Allow -Protocol Tcp -Direction Inbound -Priority 102 -SourceAddressPrefix `
+      "$ip" -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 80
+    }  
 }
 $rule2 = Allow-HTTP $myip
 
-function Allow-HTTPS{
+function Allow-HTTPS {
   [CmdletBinding()]
   Param(
       [Parameter(Mandatory)]
       [String] $ip 
   )
-  New-AzNetworkSecurityRuleConfig -Name https-rule -Description "Allow HTTPs" `
-    -Access Allow -Protocol Tcp -Direction Inbound -Priority 103 -SourceAddressPrefix `
-    "$ip" -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 443
+  if(!(Get-AzNetworkSecurityRuleConfig -Name https-rule -ErrorAction SilentlyContinue))
+    {
+      New-AzNetworkSecurityRuleConfig -Name https-rule -Description "Allow HTTPs" `
+      -Access Allow -Protocol Tcp -Direction Inbound -Priority 103 -SourceAddressPrefix `
+      "$ip" -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 443
+    }
 }
 $rule3 = Allow-HTTPS $myip
 
-function Allow-Custom{
+function Allow-Custom {
   [CmdletBinding()]
   Param(
       [Parameter(Mandatory)]
-      [String] $ip, 
+      [String] $ip,
+      [Parameter(Mandatory)] 
       [String] $port
   )
-  New-AzNetworkSecurityRuleConfig -Name custom-rule -Description "Allow Custom NSG rules" `
-    -Access Allow -Protocol Tcp -Direction Inbound -Priority 104 -SourceAddressPrefix `
-    "$ip" -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange $port;
-    Write-Output "Opening $port for $ip"
+  if(!(Get-AzNetworkSecurityRuleConfig -Name custom-rule -ErrorAction SilentlyContinue))
+    {
+      New-AzNetworkSecurityRuleConfig -Name custom-rule -Description "Allow Custom NSG rules" `
+      -Access Allow -Protocol Tcp -Direction Inbound -Priority 104 -SourceAddressPrefix `
+      "$ip" -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange $port;
+      Write-Output "Opening $port for $ip"
+    }
 }
 $rule4 = Allow-Custom -ip $myip -port 22 
 
@@ -80,39 +101,38 @@ $rule4 = Allow-Custom -ip $myip -port 22
 $nsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName -Location $location -Name NSG-FrontEnd -SecurityRules $rule1,$rule2,$rule3 
 $nsg | Set-AzNetworkSecurityGroup
 
-# Create our Networking
+# Create Networking Resources and configure
 # TODO Add parameter bindings to the function
-function Create-Networking{
+function Create-Networking {
     $frontendSubnet       = New-AzVirtualNetworkSubnetConfig -Name FrontEnd -AddressPrefix "10.0.1.0/24" -NetworkSecurityGroup $nsg
     $backendSubnet        = New-AzVirtualNetworkSubnetConfig -Name BackEnd  -AddressPrefix "10.0.2.0/24" -NetworkSecurityGroup $nsg
     $subName              = $frontendSubnet.name
     New-AzVirtualNetwork -Name $VNetName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix "10.0.0.0/16" -Subnet $frontendSubnet,$backendSubnet
-    
   }
 Create-Networking
-
-# Create the VM with the CLI since PoSh won't take the custom image string / some osProfile error
-
 function Create-VM {
   [CmdletBinding()]
   Param(
       [Parameter(Mandatory)]$image
   )
-if(!(Get-AzVm -Name $VMName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue)){ $vm = New-AzVm -Name $VMName -ResourceGroupName $resourceGroupName -Size Standard_B2ms -VirtualNetworkName $VNetName -SubnetName Subnet -SecurityGroupName $nsgName `
--ImageReferenceId $image -NetworkInterfaceDeleteOption Delete -OSDiskDeleteOption Delete # -AsJob
- }
+  if(!(Get-AzVm -Name $VMName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue))`
+    { 
+      $vm = New-AzVm -Name $VMName -ResourceGroupName $resourceGroupName `
+      -Size Standard_B2ms -VirtualNetworkName $VNetName -SubnetName Subnet -SecurityGroupName $nsgName `
+      -ImageReferenceId $image -NetworkInterfaceDeleteOption Delete -OSDiskDeleteOption Delete # -AsJob
+    }
 }
-Create-VM $image.Id
+$vm = Create-VM $image.Id
 
 # Grab IP of VM and open RDP to that address
-$ip = Get-AzPublicIpAddress -Name $pubName
-$ip.Name | mstsc 
+# $ip = Get-AzPublicIpAddress -Name $pubName
+# $ip.Name | mstsc 
 
 # Create a function to grab your test Resource Group and trash it. 
 # When you're done, just type "Clean-Up" in the terminal, powershell will grab the RG we just created and destroy it
 function Clean-Up {
-  Get-AzResourceGroup -Name $resourceGroupName | Remove-AzResourceGroup
-  Get-AzVirtualNetwork -Name $VNETName | Remove-AzVirtualNetwork
-  Get-AzNetworkSecurityGroup -Name $nsgName | Remove-AzNetworkSecurityGroup  
+  Get-AzResourceGroup -Name $resourceGroupName | Remove-AzResourceGroup -AsJob
+  Get-AzVirtualNetwork -Name $VNETName | Remove-AzVirtualNetwork -AsJob
+  Get-AzNetworkSecurityGroup -Name $nsgName | Remove-AzNetworkSecurityGroup -AsJob  
 }
 
